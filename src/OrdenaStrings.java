52ffd186eff95c1339028803f415b4c53f5e1217
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-05-22 21:13:41 -0300

    O inicio do java 8 - os lambdas

---
+++ src/OrdenaStrings.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrdenaStrings {
+	
+	public static void main(String[] args) {
+		List<String> palavras = new ArrayList<>();
+		palavras.add("alura online");
+		palavras.add("editora casa do codigo");
+		palavras.add("caelum");
+		
+		palavras.sort((s1,s2) -> Integer.compare(s1.length(), s2.length()) );
+		
+		System.out.println(palavras);
+		
+		
+		palavras.forEach(System.out::println);
+	}
+
+}
+
