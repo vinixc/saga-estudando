015b9e8002f550b29a56df4c5b861bcaea798d02
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-05-23 12:13:11 -0300

    Conhecendo fluxos reativos

---
+++ src/br/com/vini/estudando/java/onze/NotaFiscalWSClient.java
@@ -0,0 +1,15 @@
+package br.com.vini.estudando.java.onze;
+
+public class NotaFiscalWSClient {
+
+	public void enviar(NotaFiscal nf) {
+		try {
+			System.out.println("Emitindo nota fiscal");
+			Thread.sleep(5000);
+			System.out.format("Nota fiscal emitida %nEm nome de %s %nNa data de %s %n", 
+					nf.getNome(), nf.getData());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
