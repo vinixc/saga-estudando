fcadd5dfd4998045659cdbb66514f149e795f9a2
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-05-22 23:13:20 -0300

    Fabrica de collection java 9

---
+++ src/br/com/vini/estudando/factory/method/FabricaCollection.java
@@ -0,0 +1,24 @@
+package br.com.vini.estudando.factory.method;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class FabricaCollection {
+	
+	public static void main(String[] args) {
+		List<String> nomes = List.of("primeiroNome","segundoNome","terceiroNome");
+		Set.of("quartoNome");
+		Map<String, String> map = Map.of("nome","joao");
+		/*
+		ArrayList<String> nomes = new ArrayList<>();
+		nomes.add("primeiroNome");
+		nomes.add("segundoNome");
+		nomes.add("terceiroNome");
+		
+		List<String> nomesImutaveis = Collections.unmodifiableList(nomes);
+		System.out.println(nomesImutaveis);
+		 */
+	}
+
+}
