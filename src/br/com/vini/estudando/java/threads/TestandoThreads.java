e85e90d46f9383c95d2430f1c5cd457f5413adff
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-05-24 22:45:41 -0300

    Visualizando a primeira Thread

---
+++ src/br/com/vini/estudando/java/threads/TestandoThreads.java
@@ -0,0 +1,12 @@
+package br.com.vini.estudando.java.threads;
+
+public class TestandoThreads {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.out.println("isso é o thread main");
+		
+		Thread.sleep(30000);
+	}
+
+}
