c0bbef8ce83755c10b7544f36c8ab7ba285653da
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-06-02 20:24:42 -0300

    reutilizando threads.

--- src/br/com/vini/estudando/servidor/ServidorTarefa.java
+++ src/br/com/vini/estudando/servidor/ServidorTarefa.java
@@ -2,6 +2,8 @@ package br.com.vini.estudando.servidor;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServidorTarefa {
 	
@@ -10,13 +12,15 @@ public class ServidorTarefa {
 		System.out.println("INICIANDO SERVIDOR...");
 		ServerSocket servidor = new ServerSocket(12345);
 		
+//		ExecutorService threadPool = Executors.newFixedThreadPool(2);
+		ExecutorService threadPool = Executors.newCachedThreadPool();
+		
 		while(true) {
 			Socket socket = servidor.accept();
 			System.out.println("Aceitando novo cliente na porta " + socket.getPort());
 			
 			DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket);
-			Thread threadCliente = new Thread(distribuirTarefas);
-			threadCliente.start();
+			threadPool.execute(distribuirTarefas);
 		}
 		
 	}
