5b62b122b7dea400a705a56ad66a425504cb535d
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-06-02 20:39:22 -0300

    As interfaces do Pool de threads

--- src/br/com/vini/estudando/servidor/ServidorTarefa.java
+++ src/br/com/vini/estudando/servidor/ServidorTarefa.java
@@ -4,6 +4,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ServidorTarefa {
 	
@@ -15,12 +17,17 @@ public class ServidorTarefa {
 //		ExecutorService threadPool = Executors.newFixedThreadPool(2);
 		ExecutorService threadPool = Executors.newCachedThreadPool();
 		
+//		ScheduledExecutorService poolScheduled = Executors.newScheduledThreadPool(4);
+		
 		while(true) {
 			Socket socket = servidor.accept();
 			System.out.println("Aceitando novo cliente na porta " + socket.getPort());
 			
 			DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket);
 			threadPool.execute(distribuirTarefas);
+			
+			//executamos uma tarefa a cada 60 minutos
+//			poolScheduled.scheduleAtFixedRate(distribuirTarefas, 0, 60, TimeUnit.MINUTES);
 		}
 		
 	}
