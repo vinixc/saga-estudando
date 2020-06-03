3a173c5a63e5174bc58395e295293ec5531b114a
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-06-02 21:20:16 -0300

    devolvendo uma mensagem para o cliente

--- src/br/com/vini/estudando/servidor/DistribuirTarefas.java
+++ src/br/com/vini/estudando/servidor/DistribuirTarefas.java
@@ -1,5 +1,6 @@
 package br.com.vini.estudando.servidor;
 
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -19,11 +20,28 @@ public class DistribuirTarefas implements Runnable{
 			
 			Scanner entradaCliente = new Scanner(socket.getInputStream());
 			
+			PrintStream saidaCliente = new PrintStream(socket.getOutputStream());
+			
 			while(entradaCliente.hasNextLine()) {
 				String comando = entradaCliente.nextLine();
+				
+				switch(comando) {
+				case "c1":
+					saidaCliente.println("Confirmação comando c1");
+					break;
+				case "c2":
+					saidaCliente.println("Confirmação comando c2");
+					break;
+				
+				default:
+					saidaCliente.println("comando não encontrado");
+					break;
+				}
+				
 				System.out.println(comando);
 			}
 			
+			saidaCliente.close();
 			entradaCliente.close();
 		
 		} catch (Exception e) {
