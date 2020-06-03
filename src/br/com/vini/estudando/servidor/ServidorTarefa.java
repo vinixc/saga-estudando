ed2e9d74dccf9eefda6657fb7441d56b0a964937
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-06-03 10:30:41 -0300

    utilizando o AtomicBoolean abstraindo volatile

--- src/br/com/vini/estudando/servidor/ServidorTarefa.java
+++ src/br/com/vini/estudando/servidor/ServidorTarefa.java
@@ -6,12 +6,14 @@ import java.net.Socket;
 import java.net.SocketException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ServidorTarefa {
 	
 	private ServerSocket servidor;
 	private ExecutorService threadPool;
-	private volatile boolean estaRodando; //Não utilizando o cache mantido pelas threads.
+//	private volatile boolean estaRodando; //Não utilizando o cache mantido pelas threads.
+	private AtomicBoolean estaRodando; //Não utilizando o cache mantido pelas threads.
 
 	public ServidorTarefa() throws IOException {
 		System.out.println("INICIANDO SERVIDOR...");
@@ -19,18 +21,18 @@ public class ServidorTarefa {
 		
 //		ExecutorService threadPool = Executors.newFixedThreadPool(2);
 		this.threadPool = Executors.newCachedThreadPool();
-		estaRodando = true;
+		estaRodando = new AtomicBoolean(true);
 //		ScheduledExecutorService poolScheduled = Executors.newScheduledThreadPool(4);
 	}
 
 	public void parar() throws IOException {
-		estaRodando = false;
+		estaRodando = new AtomicBoolean(false);
 		servidor.close();
 		threadPool.shutdown();
 	}
 
 	public void rodar() throws IOException {
-		while(this.estaRodando) {
+		while(this.estaRodando.get()) {
 			try {
 				Socket socket = servidor.accept();
 				System.out.println("Aceitando novo cliente na porta " + socket.getPort());
